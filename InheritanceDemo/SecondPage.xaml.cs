6174176e354b85cd2abb69964a03f88aa1de05e9
Kiran Pande <kiranpande1987@gmail.com>
2019-09-18 06:12:00 +0530

    Initial check-in of module InheritanceDemo

---
+++ InheritanceDemo/SecondPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InheritanceDemo
+{
+    public partial class SecondPage : MainPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InheritanceDemo/SubPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InheritanceDemo
+{
+    public partial class SubPage : MainPage
+    {
+        public SubPage()
+        {
+            InitializeComponent();
+
+            Click.Clicked += Click_Clicked;
+        }
+
+        private void Click_Clicked(object sender, EventArgs e)
+        {
+            _ = Navigation.PushModalAsync(new SecondPage());
+        }
+    }
+}
